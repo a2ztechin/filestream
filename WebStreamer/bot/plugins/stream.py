ff1167d2ebd00d286355a5f05b5581194f35ad75
a2ztechin <96324320+a2ztechin@users.noreply.github.com>
2021-12-18 11:50:25 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -26,8 +26,9 @@ async def media_receive_handler(_, m: Message):
         file_name = file.file_name
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     stream_link = Var.URL + str(log_msg.message_id) + '/' +quote_plus(file_name) if file_name else ''
-    await m.reply_text(
+    stream_links = https://blog.textpage.xyz/surprising-physical-oddity-guys-look-for-in-women/
+await m.reply_text(
         text="`{}`".format(stream_link),
         quote=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Open', url=stream_link)]])
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Learn something interesting 🤔', url=stream_links)]])
     )
