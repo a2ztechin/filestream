288cc91eb985bb6e6fcde3693e23ec8af99babbf
a2ztechin <96324320+a2ztechin@users.noreply.github.com>
2021-12-18 11:33:23 +0530

    Update stream_routes.py

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -1,5 +1,4 @@
-# Taken from megadlbot_oss <https://github.com/eyaadh/megadlbot_oss/blob/master/mega/webserver/routes.py>
-# Thanks to Eyaadh <https://github.com/eyaadh>
+
 import re
 import time
 import math
