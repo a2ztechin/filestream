1f3c2cba18e6b3b0b31c5533c7e1e271ac93b1cc
a2ztechin <96324320+a2ztechin@users.noreply.github.com>
2021-12-18 11:34:12 +0530

    Update __init__.py

--- WebStreamer/server/__init__.py
+++ WebStreamer/server/__init__.py
@@ -1,7 +1,4 @@
-# Taken from megadlbot_oss <https://github.com/eyaadh/megadlbot_oss/blob/master/mega/webserver/__init__.py>
-# Thanks to Eyaadh <https://github.com/eyaadh>
-# This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
+
 
 from aiohttp import web
 from .stream_routes import routes
